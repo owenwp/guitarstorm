c4b0f9c620189f110479ffd120a662fe3c26989a
owen@zombieprocess.org <owen@zombieprocess.org@9eb3be25-a147-0410-a550-09e9c04672db>
2008-03-25 02:28:10 +0000

    added gpl preamble and copyright information
    git-svn-id: https://guitarstorm.googlecode.com/svn/trunk@89 9eb3be25-a147-0410-a550-09e9c04672db

--- SongPick.cpp
+++ SongPick.cpp
@@ -1,3 +1,20 @@
+/*
+	This file is part of Guitar Storm.
+	Copyright 2008 Zombie Process, Owen Pedrotti
+
+    Guitar Storm is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Guitar Storm is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Guitar Storm.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "SongPick.h"
 
 const string location = "tabs";

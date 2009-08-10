0e69e31a0edb83f7b456b92d3b5f68bd8acd5413
owen@zombieprocess.org <owen@zombieprocess.org@9eb3be25-a147-0410-a550-09e9c04672db>
2009-08-10 12:57:22 +0000

    glut window
    git-svn-id: https://guitarstorm.googlecode.com/svn/trunk@260 9eb3be25-a147-0410-a550-09e9c04672db

--- GLSLMain.cpp
+++ GLSLMain.cpp
@@ -9,7 +9,28 @@
 
 #include <GLUT/GLUT.h>
 
+void renderScene() 
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-0.5,-0.5,0.0);
+	glVertex3f(0.5,0.0,0.0);
+	glVertex3f(0.0,0.5,0.0);
+	glEnd();
+	glFlush();
+}
+
 int main(int argc, char **argv)
 {
-	glutInit(0, 0);
+	glutInit(&argc, argv);
+	
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 50); 
+	glutInitWindowSize(800, 600); 
+	glutCreateWindow("Guitar Storm GLSL Test");
+	
+	glutDisplayFunc(renderScene);
+	glutMainLoop();
+	
+	return 0;
 }

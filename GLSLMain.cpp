f0c216a2930a5becc2f403747a877090c875784d
owen@zombieprocess.org <owen@zombieprocess.org@9eb3be25-a147-0410-a550-09e9c04672db>
2009-08-10 12:57:40 +0000

    shaders
    git-svn-id: https://guitarstorm.googlecode.com/svn/trunk@262 9eb3be25-a147-0410-a550-09e9c04672db

--- GLSLMain.cpp
+++ GLSLMain.cpp
@@ -11,15 +11,76 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
+GLint v, f, p;
+
+void drawQuad()
+{
+	glBegin(GL_TRIANGLE_STRIP);
+	
+	glColor3f(1.0, 0.0, 0.0);
+	
+	glTexCoord2f(0.0, 1.0);
+	glVertex3f(-0.5, -0.5, 0.0);
+	
+	glTexCoord2f(1.0, 1.0);
+	glVertex3f(0.5, -0.5, 0.0);
+	
+	glTexCoord2f(0.0, 0.0);
+	glVertex3f(-0.5, 0.5, 0.0);
+	
+	glTexCoord2f(1.0, 0.0);
+	glVertex3f(0.5, 0.5, 0.0);
+	
+	glEnd();
+}
+
 void renderScene() 
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_TRIANGLES);
-	glVertex3f(-0.5,-0.5,0.0);
-	glVertex3f(0.5,0.0,0.0);
-	glVertex3f(0.0,0.5,0.0);
-	glEnd();
+	
+	drawQuad();
+	
 	glFlush();
+}
+
+void loadShaders()
+{
+	char *vs,*fs;
+	
+	v = glCreateShader(GL_VERTEX_SHADER);
+	f = glCreateShader(GL_FRAGMENT_SHADER);	
+	
+	//vs = fopen("Sprite.vert");
+	//fs = fopen("Sprite.frag");
+	
+	const char * vv = "void main(){gl_FrontColor = gl_Color; gl_Position = ftransform();}";
+	const char * ff = "void main(){gl_FragColor = gl_Color;}";
+	
+	glShaderSource(v, 1, &vv,NULL);
+	glShaderSource(f, 1, &ff,NULL);
+	
+	//free(vs);free(fs);
+	
+	glCompileShader(v);
+	glCompileShader(f);
+	
+	p = glCreateProgram();
+	
+	glAttachShader(p,v);
+	glAttachShader(p,f);
+	
+	glLinkProgram(p);
+	glUseProgram(p);
+}
+
+void unloadShaders()
+{
+	glDetachShader(p, v);
+	glDetachShader(p, f);
+	
+	glDeleteShader(v);
+	glDeleteShader(f);
+	glDeleteProgram(p);
 }
 
 int main(int argc, char **argv)
@@ -31,8 +92,12 @@ int main(int argc, char **argv)
 	glutInitWindowSize(800, 600); 
 	glutCreateWindow("Guitar Storm GLSL Test");
 	
+	loadShaders();
+	
 	glutDisplayFunc(renderScene);
 	glutMainLoop();
+	
+	unloadShaders();
 	
 	return 0;
 }

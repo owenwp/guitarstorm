9fa003d3b686baa7bc5bf6698ef33f55845ca667
owen@zombieprocess.org <owen@zombieprocess.org@9eb3be25-a147-0410-a550-09e9c04672db>
2009-08-10 12:57:13 +0000

    target created
    git-svn-id: https://guitarstorm.googlecode.com/svn/trunk@259 9eb3be25-a147-0410-a550-09e9c04672db

---
+++ GLSLMain.cpp
@@ -0,0 +1,15 @@
+/*
+ *  GLSLMain.cpp
+ *  guitarstorm
+ *
+ *  Created by Owen Pedrotti on 8/9/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <GLUT/GLUT.h>
+
+int main(int argc, char **argv)
+{
+	glutInit(0, 0);
+}

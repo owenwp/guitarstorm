5978615498c685599844a1e1e5abe510a594a4c1
owen@zombieprocess.org <owen@zombieprocess.org@9eb3be25-a147-0410-a550-09e9c04672db>
2009-08-10 12:58:08 +0000

    vector texture
    git-svn-id: https://guitarstorm.googlecode.com/svn/trunk@266 9eb3be25-a147-0410-a550-09e9c04672db

--- GLSLMain.cpp
+++ GLSLMain.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <math.h>
 #include <GLUT/GLUT.h>
 #include <ft2build.h>
 #include FT_FREETYPE_H
@@ -56,23 +57,21 @@ void renderScene()
 
 void makeTexture()
 {
-	unsigned char* tex = (unsigned char*)malloc(32 * 32);
+	unsigned char* tex = (unsigned char*)malloc(16 * 16);
 	
-	for(int i=0; i<32; i++)
-	for(int j=0; j<32; j++)
+	for(int i=0; i<16; i++)
+	for(int j=0; j<16; j++)
 	{
-		int x = i-16;
-		int y = j-16;
-		int dist2 = x*x + y*y;
+		int x = i-8;
+		int y = j-8;
+		float dist2 = sqrt(x*x + y*y);
 		
-		if(dist2 < 14*14)
-		{
-			tex[i+32*j] = 0xff;
-		}
-		else
-		{
-			tex[i+32*j] = 0x00;
-		}
+		dist2 -= 7;
+		dist2 *= -0.1f;
+		dist2 += 0.5f;
+		dist2 = min(1.0f, max(0.0f, dist2));
+		
+		tex[i+16*j] = (unsigned char)(dist2 * 255.0f);
 	}
 	
 	glGenTextures(1, &t);
@@ -82,7 +81,7 @@ void makeTexture()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, 32, 32, 0, GL_ALPHA, GL_UNSIGNED_BYTE, tex);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, 16, 16, 0, GL_ALPHA, GL_UNSIGNED_BYTE, tex);
 	
 	free(tex);
 }
